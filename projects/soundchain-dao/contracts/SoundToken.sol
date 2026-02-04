87a51c7cd5b8be701932f5e003301e2d4b444ba8
Márcio F. Reis <80935243+marcio-marine-corps-soldier@users.noreply.github.com>
2026-02-04 15:36:34 -0300

    Create SoundToken contract with minting and voting
    Implement ERC20Votes and Ownable functionality for SoundToken.

---
+++ projects/soundchain-dao/contracts/SoundToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SoundToken is ERC20Votes, Ownable {
+
+    constructor()
+        ERC20("SoundChain Token", "SOUND")
+        ERC20Permit("SoundChain Token")
+    {
+        _mint(msg.sender, 1_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    // overrides obrigatórios
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
